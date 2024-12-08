5547adb31e67a787c7b5c04d0c43751335c7e2e9
Pouya Gharibpour <p.gharibpour@gmail.com>
2024-12-08 10:53:34 +0330

    add logout view

--- account/urls.py
+++ account/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", view=views.index_temp, name='index_temp'),
     path("signup/", view=views.user_create, name="signup"),
     path("login/", view=LoginView.as_view(), name="login"),
+    path("logout/", view=LogoutView.as_view(), name="logout"),
 ]
