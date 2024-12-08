eb2101da2793e62d89242ef764e20b461710371f
Pouya Gharibpour <p.gharibpour@gmail.com>
2024-12-08 10:37:25 +0330

    route account urls to base path

--- mystore/urls.py
+++ mystore/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("", include("account.urls", namespace="account")),
     path("admin/", admin.site.urls),
-    path("account/", include("account.urls", namespace="account")),
 ]
