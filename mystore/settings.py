fb7caf8c4da260a210bb7960a9cd39563fc3d803
Pouya Gharibpour <p.gharibpour@gmail.com>
2024-12-08 15:26:54 +0330

    set login and logout urls

--- mystore/settings.py
+++ mystore/settings.py
@@ -126,3 +126,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
