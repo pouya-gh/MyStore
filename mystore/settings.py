72e555795ddf259147bb660f3084435c21053aa0
Pouya Gharibpour <p.gharibpour@gmail.com>
2024-12-09 10:34:11 +0330

    fix login and logout urls in settings

--- mystore/settings.py
+++ mystore/settings.py
@@ -129,5 +129,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
-LOGIN_URL = 'login'
-LOGOUT_URL = 'logout'
+LOGIN_URL = 'account:login'
+LOGOUT_URL = 'account:logout'
