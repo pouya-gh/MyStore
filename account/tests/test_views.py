21f9bcd4aeb6ddb66d08481ea79f3028bf3356d7
Pouya Gharibpour <p.gharibpour@gmail.com>
2024-12-13 10:51:34 +0330

    add tests for update user profile views

--- account/tests/test_views.py
+++ account/tests/test_views.py
@@ -1,14 +1,18 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse
+from datetime import date
+from ..forms import MyUserChangeForm, CustomerProfileForm
+from ..models import CustomerProfile
 
-
-class ViewsTests(TestCase):
+class SignupViewTests(TestCase):
     @classmethod
     def setUpTestData(cls):
         user_model = get_user_model()
 
-        user_model.objects.create(username="user1", password="user1user1")
+        user1 = user_model.objects.create_user(username="user1", password="user1user1")
+        user1.save()
 
 
     def test_signup_page_exists(self):
@@ -24,7 +28,7 @@ class ViewsTests(TestCase):
     def test_signup_url_uses_correct_template(self):
         response = self.client.get(reverse("account:signup"))
         self.assertTemplateUsed(response, "registration/signup.html")
-        self.assertIn("form", response.context)
+        self.assertIsInstance(response.context["form"], UserCreationForm)
 
     def test_signup_request_works_with_valid_data(self):
         signup_data = {"username":"user2", "password1":"user2user2", "password2":"user2user2"}
@@ -32,3 +36,77 @@ class ViewsTests(TestCase):
         user_count = get_user_model().objects.all().count()
         self.assertRedirects(response, "/")
         self.assertEqual(user_count, 2)
+
+class UpdateUserViewTests(TestCase):
+    valid_profile_form_data = {"first_name":"fname",
+                                "last_name":"lname",
+                                "email":"user1@mail.com",
+                                "social_code":"1111111111",
+                                "birth_date":date(1990, 10, 10),
+                                "country":"IR",
+                                "province":"Tehran",
+                                "city":"Tehran",
+                                "address":"123 fake street",
+                                "phone_number":"09875430512",
+                                "phone_number2":"09875430555",
+                                }
+
+    @classmethod
+    def setUpTestData(cls):
+        user_model = get_user_model()
+
+        user1 = user_model.objects.create_user(username="user1", password="user1user1")
+        user1.save()
+
+    
+    def test_page_exits_if_logged_in(self):
+        self.client.login(username="user1", password="user1user1")
+        response = self.client.get("/profile/update")
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_redirects_to_login_if_not_logged_in(self):
+        response = self.client.get("/profile/update")
+        self.assertTrue(response.url.startswith("/login"))
+
+
+    def test_update_profile_url_has_correct_name(self):
+        self.client.login(username="user1", password="user1user1")
+        response = self.client.get(reverse("account:cutomer_profile_set"))
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_update_profile_uses_correct_template_with_get_request(self):
+        self.client.login(username="user1", password="user1user1")
+        response = self.client.get(reverse("account:cutomer_profile_set"))
+        self.assertTemplateUsed(response, "account/customerprofile/form.html")
+        self.assertIsInstance(response.context["form"], CustomerProfileForm)
+        self.assertIsInstance(response.context["auth_user_form"], MyUserChangeForm)
+
+    def test_update_profile_request_works_with_valid_data(self):
+        self.client.login(username="user1", password="user1user1")
+        response = self.client.post(reverse("account:cutomer_profile_set"), data=self.valid_profile_form_data)
+        self.assertRedirects(response, "/")
+
+    def test_successful_update_profile_request_creates_profile_if_it_doesnt_exist(self):
+        self.client.login(username="user1", password="user1user1")
+        initial_profile_count = CustomerProfile.objects.all().count()
+        response = self.client.post(reverse("account:cutomer_profile_set"), data=self.valid_profile_form_data)
+        final_profile_count = CustomerProfile.objects.all().count()
+        self.assertEqual(initial_profile_count, 0)
+        self.assertEqual(final_profile_count, 1)
+
+    def test_update_profile_request_reloads_form_with_invalid_data(self):
+        self.client.login(username="user1", password="user1user1")
+        data = {"first_name":"fname",
+                "last_name":"lname",
+                "email":"user1@mail.com",
+                "social_code":"",
+                "birth_date":date(1990, 10, 10),
+                "phone_number":"09875430512",
+                "phone_number2":"09875430555",
+                }
+        response = self.client.post(reverse("account:cutomer_profile_set"), data=data)
+        self.assertTemplateUsed(response, "account/customerprofile/form.html")
+        self.assertIsInstance(response.context["form"], CustomerProfileForm)
+        self.assertIsInstance(response.context["auth_user_form"], MyUserChangeForm)
