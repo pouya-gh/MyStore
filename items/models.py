87432fa543eb7c6cff844cb3311aa3632318db4f
Pouya Gharibpour <p.gharibpour@gmail.com>
2024-12-16 10:58:01 +0330

    add Item model

--- items/models.py
+++ items/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from account.models import ProviderProfile
+from django.conf import settings
+from django.utils import timezone
 
-# Create your models here.
+def _validate_item_properties(value):
+        for v in value.items():
+            if not isinstance(v,list):
+                raise ValidationError("properties values must only be list of strings")
+            if not all(isinstance(m, str) for m in v):
+                    raise ValidationError("properties values must only be list of strings")
+
+class Item(models.Model):
+    class ItemSubmissionStatus(models.TextChoices):
+        VERIFIED = 'VF', 'Verified'
+        PENDING = 'PN', 'Verfication pending'
+        DECLINED = 'DC', 'Declined'
+
+    name = models.CharField(max_length=250)
+    slug = models.CharField(max_length=250, unique=True)
+    provider = models.ForeignKey(ProviderProfile, on_delete=models.CASCADE)
+    submitted_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="submitted_items")
+    properties = models.JSONField(validators=[_validate_item_properties])
+    description = models.TextField()
+    remaining_items = models.PositiveIntegerField(default=0, blank=False)
+    created = models.DateTimeField(auto_now_add=True)
+    publish = models.DateTimeField(default=timezone.now)
+    updated = models.DateTimeField(auto_now=True)
+    submission_status = models.CharField(max_length=2, choices=ItemSubmissionStatus, default=ItemSubmissionStatus.PENDING)
+    submission_review_message = models.TextField(null=True)
+
+    class Meta:
+        ordering = ["-publish", "slug"]
+        indexes = [
+            models.Index(fields=['-publish']),
+             models.Index(fields=['slug']),
+        ]
+
+    def __str__(self):
+         return self.name
