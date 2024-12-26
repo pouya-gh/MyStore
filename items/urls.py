891be9cd3e5cf08fa4d2226410a03a671b060421
Pouya Gharibpour <p.gharibpour@gmail.com>
2024-12-26 16:06:30 +0330

    add detail view for items

--- items/urls.py
+++ items/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = "items"
 urlpatterns = [
     path("", view=views.ItemListView.as_view(), name="items_list"),
+    path("<pk>/", view=views.ItemDetailView.as_view(), name="item_details"),
 ]
--- items/views.py
+++ items/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Item
 
 
@@ -7,3 +8,9 @@ class ItemListView(ListView):
     model = Item
     template_name = "items/list.html"
     context_object_name = "items"
+
+
+class ItemDetailView(DetailView):
+    model = Item
+    template_name = "items/detail.html"
+    context_object_name = "item"
