e5bde5d3d6b030a0d1d02d1b4d087aaf60c22f3c
Pouya Gharibpour <p.gharibpour@gmail.com>
2024-12-28 21:43:56 +0330

    add item create view

--- items/urls.py
+++ items/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = "items"
 urlpatterns = [
     path("", view=views.ItemListView.as_view(), name="items_list"),
+    path("new", view=views.ItemCreateView.as_view(), name="item_create"),
     path("<pk>", view=views.ItemDetailView.as_view(), name="item_details"),
     path("update/<pk>", view=views.ItemUpdateView.as_view(), name="item_update"),
     path("delete/<pk>", view=views.ItemDeleteView.as_view(), name="item_delete"),
--- items/views.py
+++ items/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import DeleteView, UpdateView
+from django.views.generic.edit import DeleteView, UpdateView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Item
 from .forms import ItemForm
@@ -33,3 +33,19 @@ class ItemDeleteView(LoginRequiredMixin, DeleteView):
     
     def get_queryset(self):
         return self.request.user.submitted_items
+    
+
+class ItemCreateView(LoginRequiredMixin, CreateView):
+    model = Item
+    template_name = "items/form.html"
+    form_class = ItemForm
+
+    def get_queryset(self):
+        return self.request.user.submitted_items
+    
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.submitted_by = self.request.user
+        self.object.save()
+        return super().form_valid(form)
+    
