72262d615acf0e02631c016f5c79c8de57a77309
Pouya Gharibpour <p.gharibpour@gmail.com>
2025-01-01 14:26:50 +0330

    add ordering and index to category model. i always forget this

--- items/models.py
+++ items/models.py
@@ -12,6 +12,10 @@ class Category(models.Model):
     parent = models.ForeignKey(
         "self", on_delete=models.CASCADE, null=True, blank=True, related_name="sub_categories")
 
+    class Meta:
+        ordering = ["slug"]
+        indexes = [models.Index(fields=["slug"])]
+
     def __str__(self):
         return self.name
 
