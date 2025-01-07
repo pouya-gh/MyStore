d4164db479aea7763979decad5010082e353beea
Pouya Gharibpour <p.gharibpour@gmail.com>
2025-01-07 10:57:00 +0330

    add order and orderitem models

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ["order", "item", "quantity", "created_at"]
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["id", "customer", "status"]
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+from items.models import Item
+
+import uuid
+
+class OrderItem(models.Model):
+    order = models.ForeignKey("Order",
+                              on_delete=models.CASCADE,
+                              related_name="order_items")
+    item = models.ForeignKey(Item,
+                             on_delete=models.PROTECT)
+    sku = models.SlugField(max_length=255,
+                           blank=False,
+                           null=False,
+                           default=None)
+    quantity = models.PositiveIntegerField(null=False, blank=False)
+    properties = models.JSONField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order for {self.quantity} of {self.item}"
+
+
+class Order(models.Model):
+    class OrderStatus(models.TextChoices):
+        PENDING = "PN", "Pending"
+        PAYMENT_ACCEPTED = "PA", "Payment Accepted"
+        PAYMENT_DECLINED = "PD", "Payment Declined"
+        CANCELED = "CN", "Canceled"
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    customer = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                 on_delete=models.PROTECT,
+                                 related_name="orders")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2,
+                              choices=OrderStatus,
+                              default=OrderStatus.PENDING)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return self.id
