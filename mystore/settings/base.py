382e9ebbbaa807c113e174493c99edb3c3436162
Pouya Gharibpour <p.gharibpour@gmail.com>
2025-01-07 10:56:43 +0330

    remove old unused settings file

--- mystore/settings/base.py
+++ mystore/settings/base.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "items.apps.ItemsConfig",
+    "orders.apps.OrdersConfig",
+    "payment.apps.PaymentConfig",
 ]
 
 MIDDLEWARE = [
