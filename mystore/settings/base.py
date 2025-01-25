f77321850aeb825c88bd9e996d8fbda6365cc067
Pouya Gharibpour <p.gharibpour@gmail.com>
2025-01-25 16:08:16 +0330

    add bootstrap 5

--- mystore/settings/base.py
+++ mystore/settings/base.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_bootstrap5",
     "items.apps.ItemsConfig",
     "orders.apps.OrdersConfig",
     "payment.apps.PaymentConfig",
