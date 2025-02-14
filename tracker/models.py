0c8f8f5d43672f5c294f75cd010dee1ab13c2e10
Pouya Gharibpour <p.gharibpour@gmail.com>
2025-02-14 12:53:44 +0330

    add sitetrackermodel

--- tracker/models.py
+++ tracker/models.py
@@ -1,2 +1,14 @@
 from django.db import models
 
+class SiteVisitTracker(models.Model):
+    ip = models.GenericIPAddressField()
+    counter = models.PositiveIntegerField(default=1)
+    first_encounter = models.DateTimeField(auto_now_add=True)
+    last_encounter = models.DateTimeField(auto_now=True)
+    location = models.CharField(max_length=50, default="")
+
+    class Meta:
+        ordering = ["-last_encounter", "-first_encounter"]
+        indexes = [
+            models.Index(fields=["-last_encounter"])
+        ]
