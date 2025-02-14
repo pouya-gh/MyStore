3e9ea7761ec580141d2484e5f0d9cf8c1962f8f7
Pouya Gharibpour <p.gharibpour@gmail.com>
2025-02-14 13:31:15 +0330

    add SiteVisitTracker model to admin page

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import SiteVisitTracker
+
+@admin.register(SiteVisitTracker)
+class SiteVisitTrackerAdmin(admin.ModelAdmin):
+    list_display = ["ip", "counter", "last_encounter", "location"]
